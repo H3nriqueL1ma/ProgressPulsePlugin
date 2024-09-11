4822644a84b402a8ba32cee9906c981c1e75416b
H3nriqueL1ma <henriquecarlos361893@gmail.com>
2024-09-11 17:29:18 -0300

    feat: criação do projeto do plugin

---
+++ src/main/java/com/github/h3nriquel1ma/progresspulse/ProgressPulse.java
@@ -0,0 +1,18 @@
+package com.github.h3nriquel1ma.progresspulse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class ProgressPulse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
