1fbebe08be73447a0e413622915a4b7eab2175fd
user_acronus <user_acronus@senacsp.edu.br>
2024-09-13 17:46:05 -0300

    feat: teste de evento de combate com mensagem no chat

--- src/main/java/com/github/h3nriquel1ma/progressPulsePlugin/Events/CombatEventListener.java
+++ src/main/java/com/github/h3nriquel1ma/progressPulsePlugin/Events/CombatEventListener.java
@@ -1,13 +1,21 @@
 package com.github.h3nriquel1ma.progressPulsePlugin.Events;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class CombatEventListener implements Listener {
 
     @EventHandler
-    public void onTest(PlayerItemDamageEvent event) {
-        //event.
+    public void onTest(EntityDamageByEntityEvent event) {
+        Entity entity = event.getDamager();
+
+        if (entity instanceof Player) {
+            Player player = (Player) event.getEntity();
+
+            player.sendMessage("Você atacou uma entidade!");
+        }
     }
 }
--- src/main/java/com/github/h3nriquel1ma/progressPulsePlugin/ProgressPulsePlugin.java
+++ src/main/java/com/github/h3nriquel1ma/progressPulsePlugin/ProgressPulsePlugin.java
@@ -1,5 +1,6 @@
 package com.github.h3nriquel1ma.progressPulsePlugin;
 
+import com.github.h3nriquel1ma.progressPulsePlugin.Events.CombatEventListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ProgressPulsePlugin extends JavaPlugin {
@@ -7,12 +8,12 @@ public final class ProgressPulsePlugin extends JavaPlugin {
     // Utilizado para registrar ouvintes de eventos.
     @Override
     public void onEnable() {
-
-
+        getLogger().info("ProgressPulse has been enabled!");
+        getServer().getPluginManager().registerEvents(new CombatEventListener(), this);
     }
 
     @Override
     public void onDisable() {
-
+        getLogger().info("ProgressPulse has been disabled!");
     }
 }
